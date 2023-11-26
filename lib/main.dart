f93a65c56fc1ebf867814ea1b196306c5987c7a1
Temel00 <temelaudio@gmail.com>
2023-11-26 15:08:24 -0800

    Added ProfileFieldPage to create conditional pages

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(children: <Widget>[
-      SizedBox(height: 30),
+      const SizedBox(height: 30),
       Text(
         "Edit Profile",
         style: Theme.of(
@@ -90,37 +90,72 @@ class HomeScreen extends StatelessWidget {
           children: ListTile.divideTiles(context: context, tiles: [
             ListTile(
               onTap: () {
-                debugPrint("List Tile Name was clicked");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileFieldPage(
+                        initialValue1: user.firstName,
+                        initialValue2: user.lastName,
+                        field: "name"),
+                  ),
+                );
               },
               trailing: const Icon(Icons.arrow_forward_ios_rounded),
               contentPadding: EdgeInsets.zero,
               title: ProfileField(
-                  field: "Name",
+                  fieldLabel: "Name",
                   fieldValue: "${user.firstName} ${user.lastName}"),
             ),
             ListTile(
               onTap: () {
-                debugPrint("List Tile Phone was clicked");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileFieldPage(
+                      initialValue1: user.phone,
+                      initialValue2: '',
+                      field: "phone",
+                    ),
+                  ),
+                );
               },
               trailing: const Icon(Icons.arrow_forward_ios_rounded),
               contentPadding: EdgeInsets.zero,
-              title: ProfileField(field: "Phone", fieldValue: user.phone),
+              title: ProfileField(fieldLabel: "Phone", fieldValue: user.phone),
             ),
             ListTile(
               onTap: () {
-                debugPrint("List Tile Email was clicked");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileFieldPage(
+                        initialValue1: user.email,
+                        initialValue2: '',
+                        field: "email"),
+                  ),
+                );
               },
               trailing: const Icon(Icons.arrow_forward_ios_rounded),
               contentPadding: EdgeInsets.zero,
-              title: ProfileField(field: "Email", fieldValue: user.email),
+              title: ProfileField(fieldLabel: "Email", fieldValue: user.email),
             ),
             ListTile(
               onTap: () {
-                debugPrint("List Tile Bio was clicked");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileFieldPage(
+                      initialValue1: user.bio,
+                      initialValue2: '',
+                      field: "bio",
+                    ),
+                  ),
+                );
               },
               trailing: const Icon(Icons.arrow_forward_ios_rounded),
               contentPadding: EdgeInsets.zero,
-              title: ProfileField(field: "Bio", fieldValue: user.bio),
+              title: ProfileField(
+                  fieldLabel: "Tell us about yourself", fieldValue: user.bio),
             ),
           ]).toList(),
         ),
@@ -131,9 +166,9 @@ class HomeScreen extends StatelessWidget {
 
 class ProfileField extends StatelessWidget {
   const ProfileField(
-      {super.key, required this.field, required this.fieldValue});
+      {super.key, required this.fieldLabel, required this.fieldValue});
 
-  final String field;
+  final String fieldLabel;
   final String fieldValue;
 
   @override
@@ -143,7 +178,7 @@ class ProfileField extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          field,
+          fieldLabel,
           style: Theme.of(
             context,
           ).textTheme.labelMedium,
@@ -153,6 +188,185 @@ class ProfileField extends StatelessWidget {
               context,
             ).textTheme.bodyMedium),
       ],
+    );
+  }
+}
+
+class ProfileFieldPage extends StatefulWidget {
+  const ProfileFieldPage(
+      {super.key,
+      required this.initialValue1,
+      required this.initialValue2,
+      required this.field});
+
+  final String initialValue1, initialValue2, field;
+
+  @override
+  State<ProfileFieldPage> createState() => _ProfileFieldPageState();
+}
+
+class _ProfileFieldPageState extends State<ProfileFieldPage> {
+  late TextEditingController _controller1;
+  late TextEditingController _controller2;
+  late GlobalKey<FormState> _formKey;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller1 = TextEditingController(text: widget.initialValue1);
+    _controller2 = TextEditingController(text: widget.initialValue2);
+    _formKey = GlobalKey<FormState>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  if (widget.field == "name") const Text("What's your name?"),
+                  if (widget.field == "phone")
+                    const Text("What's your phone number?"),
+                  if (widget.field == "email") const Text("What's your email?"),
+                  if (widget.field == "bio")
+                    const Text("What type of passenger are you?"),
+                  Row(
+                    children: <Widget>[
+                      if (widget.field == "name")
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)),
+                            child: TextFormField(
+                              keyboardType: TextInputType.name,
+                              controller: _controller1,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Last Name",
+                                contentPadding: EdgeInsets.all(16),
+                              ),
+                              style: const TextStyle(height: 2),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Field cannot be empty';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                      if (widget.field == "phone")
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)),
+                            child: TextFormField(
+                              keyboardType: TextInputType.phone,
+                              controller: _controller1,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Last Name",
+                                contentPadding: EdgeInsets.all(16),
+                              ),
+                              style: const TextStyle(height: 2),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Field cannot be empty';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                      if (widget.field == "email")
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)),
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              controller: _controller1,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Last Name",
+                                contentPadding: EdgeInsets.all(16),
+                              ),
+                              style: const TextStyle(height: 2),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Field cannot be empty';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                      if (widget.field == "bio")
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)),
+                            child: TextFormField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              controller: _controller1,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Last Name",
+                                contentPadding: EdgeInsets.all(16),
+                              ),
+                              style: const TextStyle(height: 2),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Field cannot be empty';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                      const SizedBox(width: 20),
+                      if (widget.field == "name")
+                        Flexible(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey)),
+                            child: TextFormField(
+                              keyboardType: TextInputType.name,
+                              controller: _controller2,
+                              decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Last Name",
+                                contentPadding: EdgeInsets.all(16),
+                              ),
+                              style: const TextStyle(height: 2),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Field cannot be empty';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        // Validate successful, pass the updated value back
+                        Navigator.pop(context, _controller1.text);
+                      }
+                    },
+                    child: const Text('Update'),
+                  ),
+                ],
+              ))),
     );
   }
 }
