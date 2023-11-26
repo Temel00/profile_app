b2830b8931374bd439265c0b0f1bc5053686da82
Temel00 <temelaudio@gmail.com>
2023-11-25 19:00:30 -0800

    Added Title Text and Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -17,10 +17,12 @@ class User {
 }
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -44,38 +46,86 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Edit Profile'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: ListView(
-            children: ListTile.divideTiles(context: context, tiles: [
-              ListTile(
-                trailing: Icon(Icons.arrow_forward_ios_rounded),
-                contentPadding: EdgeInsets.zero,
-                title: ProfileField(
-                    field: "Name",
-                    fieldValue: "${user.firstName} ${user.lastName}"),
-              ),
-              ListTile(
-                trailing: Icon(Icons.arrow_forward_ios_rounded),
-                contentPadding: EdgeInsets.zero,
-                title: ProfileField(field: "Phone", fieldValue: user.phone),
-              ),
-              ListTile(
-                trailing: Icon(Icons.arrow_forward_ios_rounded),
-                contentPadding: EdgeInsets.zero,
-                title: ProfileField(field: "Email", fieldValue: user.email),
-              ),
-              ListTile(
-                trailing: Icon(Icons.arrow_forward_ios_rounded),
-                contentPadding: EdgeInsets.zero,
-                title: ProfileField(field: "Bio", fieldValue: user.bio),
-              ),
-            ]).toList(),
+        body: Column(children: <Widget>[
+      SizedBox(height: 30),
+      Text(
+        "Edit Profile",
+        style: Theme.of(
+          context,
+        ).textTheme.displayMedium,
+      ),
+      const SizedBox(height: 20),
+      Stack(
+        children: [
+          CircleAvatar(
+            radius: 64,
+            backgroundColor: Colors.blue,
+            child: Padding(
+              padding: const EdgeInsets.all(4),
+              child: ClipOval(
+                  child: Image.network('https://i.pravatar.cc/150?img=11')),
+            ),
           ),
-        ));
+          Positioned(
+            top: 1,
+            right: 1,
+            child: ElevatedButton(
+                onPressed: () {
+                  debugPrint("Edit Button was clicked");
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(10),
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.blue,
+                ),
+                child: const Icon(Icons.edit, color: Colors.blue)),
+          ),
+        ],
+      ),
+      Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: ListView(
+          shrinkWrap: true,
+          children: ListTile.divideTiles(context: context, tiles: [
+            ListTile(
+              onTap: () {
+                debugPrint("List Tile Name was clicked");
+              },
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              contentPadding: EdgeInsets.zero,
+              title: ProfileField(
+                  field: "Name",
+                  fieldValue: "${user.firstName} ${user.lastName}"),
+            ),
+            ListTile(
+              onTap: () {
+                debugPrint("List Tile Phone was clicked");
+              },
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              contentPadding: EdgeInsets.zero,
+              title: ProfileField(field: "Phone", fieldValue: user.phone),
+            ),
+            ListTile(
+              onTap: () {
+                debugPrint("List Tile Email was clicked");
+              },
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              contentPadding: EdgeInsets.zero,
+              title: ProfileField(field: "Email", fieldValue: user.email),
+            ),
+            ListTile(
+              onTap: () {
+                debugPrint("List Tile Bio was clicked");
+              },
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              contentPadding: EdgeInsets.zero,
+              title: ProfileField(field: "Bio", fieldValue: user.bio),
+            ),
+          ]).toList(),
+        ),
+      ),
+    ]));
   }
 }
 
