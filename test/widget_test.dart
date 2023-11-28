828cbbcd17c6b6dc634c950f2d688f11eedd0b26
Temel00 <temelaudio@gmail.com>
2023-11-27 17:20:25 -0800

    Added tests for all ProfileFieldPages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -34,32 +34,35 @@ void main() {
 
       // Test for Correct Phone label and inital Phone value
       final phoneFinder = find.text("Phone");
-      final phoneInit = find.widgetWithText(ListTile, "208-206-5039");
+      final phoneTile = find.widgetWithText(ListTile, "208-206-5039");
       expect(phoneFinder, findsOneWidget);
-      expect(phoneInit, findsOneWidget);
+      expect(phoneTile, findsOneWidget);
 
       // Test for Correct Email label and inital Email value
       final emailFinder = find.text("Email");
-      final emailInit = find.widgetWithText(ListTile, "micahsmith@gmail.com");
+      final emailTile = find.widgetWithText(ListTile, "micahsmith@gmail.com");
       expect(emailFinder, findsOneWidget);
-      expect(emailInit, findsOneWidget);
+      expect(emailTile, findsOneWidget);
 
       // Test for Correct Bio label and inital Bio value
       final bioFinder = find.text("Tell us about yourself");
-      final bioInit = find.widgetWithText(ListTile,
+      final bioTile = find.widgetWithText(ListTile,
           "Hi my name is Mica Smith. I am from Mesa but go to school in Salt Lake City. I make this drive all the time and have plenty");
       expect(bioFinder, findsOneWidget);
-      expect(bioInit, findsOneWidget);
+      expect(bioTile, findsOneWidget);
     });
-
-    testWidgets('Testing for Name Page form fields', (tester) async {
+  });
+  group('Testing ProfileFieldPages', () {
+    testWidgets(
+        'Testing correct conditional form field and user input for Name Page',
+        (tester) async {
       await tester.pumpWidget(createHomeScreen());
 
       // Finding the List Tile with the User's name
-      final nameInit = find.widgetWithText(ListTile, "Micah Smith");
+      final nameTile = find.widgetWithText(ListTile, "Micah Smith");
 
       // Tap on the ListTile to Navigate to the ProfileFieldPage for Name
-      await tester.tap(nameInit);
+      await tester.tap(nameTile);
       await tester.pumpAndSettle();
 
       // Test to make sure you have a correct ProfileFieldPage Widget
@@ -70,6 +73,121 @@ void main() {
       final lNameField = find.widgetWithText(TextFormField, "Smith");
       expect(fNameField, findsOneWidget);
       expect(lNameField, findsOneWidget);
+
+      // Add new text to both first and last name fields
+      await tester.enterText(fNameField, "first test");
+      await tester.enterText(lNameField, "last test");
+
+      // Find and tap the update button
+      final updateButton = find.widgetWithText(ElevatedButton, "Update");
+      await tester.tap(updateButton);
+      await tester.pumpAndSettle();
+
+      // Find new Tile based on updated first and last name text
+      final newNameTile = find.widgetWithText(ListTile, "first test last test");
+      expect(newNameTile, findsOneWidget);
+    });
+
+    // Testing for Phone Page
+    testWidgets(
+        'Testing correct conditional form field and user input for Phone Page',
+        (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+
+      // Finding the List Tile with the User's phone
+      final phoneTile = find.widgetWithText(ListTile, "208-206-5039");
+
+      // Tap on the ListTile to Navigate to the ProfileFieldPage for Phone
+      await tester.tap(phoneTile);
+      await tester.pumpAndSettle();
+
+      // Test to make sure you have a correct ProfileFieldPage Widget
+      expect(find.byType(ProfileFieldPage), findsOneWidget);
+
+      // Test for TextFormField that holds phone
+      final phoneField = find.widgetWithText(TextFormField, "208-206-5039");
+      expect(phoneField, findsOneWidget);
+
+      // Add text to the phone form field
+      await tester.enterText(phoneField, "phone field test");
+
+      // Grab the updateButton and Tap it
+      final updateButton = find.widgetWithText(ElevatedButton, "Update");
+      await tester.tap(updateButton);
+      await tester.pumpAndSettle();
+
+      // Find the tile with new phone text
+      final newPhoneTile = find.widgetWithText(ListTile, "phone field test");
+      expect(newPhoneTile, findsOneWidget);
+    });
+
+    // Testing for Email Page
+    testWidgets(
+        'Testing correct conditional form field and user input for Email Page',
+        (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+
+      // Finding the List Tile with the User's email
+      final emailTile = find.widgetWithText(ListTile, "micahsmith@gmail.com");
+
+      // Tap on the ListTile to Navigate to the ProfileFieldPage for Email
+      await tester.tap(emailTile);
+      await tester.pumpAndSettle();
+
+      // Test to make sure you have a correct ProfileFieldPage Widget
+      expect(find.byType(ProfileFieldPage), findsOneWidget);
+
+      // Test for TextFormField that holds email
+      final emailField =
+          find.widgetWithText(TextFormField, "micahsmith@gmail.com");
+      expect(emailField, findsOneWidget);
+
+      // Add text to the email form field
+      await tester.enterText(emailField, "email field test");
+
+      // Grab the updateButton and Tap it
+      final updateButton = find.widgetWithText(ElevatedButton, "Update");
+      await tester.tap(updateButton);
+      await tester.pumpAndSettle();
+
+      // Find the tile with new email text
+      final newEmailTile = find.widgetWithText(ListTile, "email field test");
+      expect(newEmailTile, findsOneWidget);
+    });
+
+    // Testing for Bio Page
+    testWidgets(
+        'Testing correct conditional form field and user input for Bio Page',
+        (tester) async {
+      await tester.pumpWidget(createHomeScreen());
+
+      // Finding the List Tile with the User's bio
+      final bioTile = find.widgetWithText(ListTile,
+          "Hi my name is Mica Smith. I am from Mesa but go to school in Salt Lake City. I make this drive all the time and have plenty");
+
+      // Tap on the ListTile to Navigate to the ProfileFieldPage for Bio
+      await tester.tap(bioTile);
+      await tester.pumpAndSettle();
+
+      // Test to make sure you have a correct ProfileFieldPage Widget
+      expect(find.byType(ProfileFieldPage), findsOneWidget);
+
+      // Test for TextFormField that holds bio
+      final bioField = find.widgetWithText(TextFormField,
+          "Hi my name is Mica Smith. I am from Mesa but go to school in Salt Lake City. I make this drive all the time and have plenty");
+      expect(bioField, findsOneWidget);
+
+      // Add text to the bio form field
+      await tester.enterText(bioField, "bio field test");
+
+      // Grab the updateButton and Tap it
+      final updateButton = find.widgetWithText(ElevatedButton, "Update");
+      await tester.tap(updateButton);
+      await tester.pumpAndSettle();
+
+      // Find the tile with new bio text
+      final newBioTile = find.widgetWithText(ListTile, "bio field test");
+      expect(newBioTile, findsOneWidget);
     });
   });
 }
